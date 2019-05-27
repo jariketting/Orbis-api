7cbe444c11416122dea96185bf77de052e594263
Jari <personal@jariketting.com>
2019-05-27 13:26:42 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+//Display errors.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//set json content type
+header('Content-Type: application/json');
+
+//example class for json return
+class Example {
+	public $message; //must be public for json to encode and return it.
+	
+	//constuctor
+	public function __construct(string $message) {
+		$this->message = $message; //set test variable
+	}
+}
+
+$obj = new Example('Hello world!'); //create object
+
+echo json_encode($obj); //retun json encoded object to user,
+
+//By Jari Ketting
