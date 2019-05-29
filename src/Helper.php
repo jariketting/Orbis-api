79be03fbda92d98710adcc2072c20ae462f6f7ca
Jari <personal@jariketting.com>
2019-05-29 14:43:14 +0200

    Add PHPDoc

--- src/Helper.php
+++ src/Helper.php
@@ -1,12 +1,18 @@
 <?php
-
-
 namespace Orbis;
 
-
+/**
+ * Class contains helper functions used in this project (acts like a functions.php file)
+ *
+ * Class Helper
+ * @package Orbis
+ */
 class Helper
 {
+    /**
+     * Just a test function to verify everything's working :-)
+     */
     static function test() : void {
-        echo 'This is a test';
+        echo 'This is a test'; //return string
     }
 }
