cf8248dd85155fd5c5cf540b69dbc00e2ff348db
Jari <personal@jariketting.com>
2019-05-29 13:53:04 +0200

    Add test stuff to index

--- public/index.php
+++ public/index.php
@@ -1,29 +1,18 @@
 <?php
-//Display errors.
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+require_once '../vendor/autoload.php';
+use Orbis\Config;
 
+//stop application if config is not loaded
+if(!Config::loadConfig('../config.ini'))
+    die();
 
-
-//example class for json return
-class Example {
-	public $message; //must be public for json to encode and return it.
-	
-	//constuctor
-	public function __construct(string $message) {
-		$this->message = $message; //set test variable
-	}
+//show errors when in debug mode
+if(Config::getConfig()['SETTINGS']['debug']) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 }
 
-$obj = new Example('Hello world!'); //create object
+//header('Content-Type: application/json');
 
-if(isset($_GET['phpinfo']))
-    phpinfo();
-else {
-    //set json content type
-    header('Content-Type: application/json');
-    echo json_encode($obj); //retun json encoded object to user,
-}
-
-//By Jari Ketting
+Orbis\Helper::test();
