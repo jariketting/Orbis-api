579fa4a9f3b62bc890b8cd328f0e4890ec639dac
Jari <personal@jariketting.com>
2019-05-29 13:40:14 +0200

    Update

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Orbis\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInited5d970ae004c0b5383c9691f3a0451b
 {
+    public static $prefixLengthsPsr4 = array (
+        'O' => 
+        array (
+            'Orbis\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Orbis\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInited5d970ae004c0b5383c9691f3a0451b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInited5d970ae004c0b5383c9691f3a0451b::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
