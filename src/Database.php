65fc0ae6fd491fe88b68e36241d708745ddebc6d
Jari <personal@jariketting.com>
2019-05-29 14:46:15 +0200

    Add PHPDoc

--- src/Database.php
+++ src/Database.php
@@ -1,26 +1,41 @@
 <?php
-
-
 namespace Orbis;
-
 
 use PDO;
 use PDOException;
 
+/**
+ * Database handler
+ *
+ * Class Database
+ * @package Orbis
+ */
 class Database
 {
-    private static $_connection = null;
+    private static $_connection = null; //stores the database connection
 
+    /**
+     * Init the database connection, has to be done once before trying to access the database.
+     *
+     * @param string $host hostname of database
+     * @param string $db name of database
+     * @param string $user username of database
+     * @param string $passwd password of database
+     *
+     * @return bool true if successful connected, false on any error
+     */
     public static function init(string $host, string $db, string $user, string $passwd) : bool {
+        //try to connect, catch any errors
         try {
+            //create new PDO database connection with given credentials
             self::$_connection = new PDO("mysql:host=$host;dbname=$db", $user, $passwd);
 
             // set the PDO error mode to exception
             self::$_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            return true;
+            return true; //successful connection has been made
         } catch(PDOException $e) {
-            return false;
+            return false; //TODO convert to fatal json error for APP compatibility
         }
     }
 }
