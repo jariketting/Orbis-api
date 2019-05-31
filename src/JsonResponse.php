db8d7558cdd30ce4e0b38d9b022d10c778d28c99
Jari <personal@jariketting.com>
2019-05-31 13:47:38 +0200

    Add json response class

---
+++ src/JsonResponse.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Orbis;
+
+
+class JsonResponse
+{
+    /*
+     * Storing error stuff
+     */
+    private $_error = false,     //true if error occurred
+            $_errorTitle = '',   //title of error
+            $_errorDetail = '';  //detailed error description
+
+    private $_responseCode = 200; //response code
+
+    private $_data = []; //data returned
+
+    public function error(string $title, string $detail) : void {
+        $this->_error = true;
+        $this->_errorTitle = $title;
+        $this->_errorDetail = $detail;
+    }
+
+    public function setResponseCode(int $code) : void {
+        $this->_responseCode = $code;
+    }
+
+    public function setData($data) : void {
+        $this->_data = $data;
+    }
+
+    public function print() : void {
+        $json = json_encode([
+            'error' => [
+                'error'     => $this->_error,
+                'title'     => $this->_errorTitle,
+                'detail'    => $this->_errorDetail
+            ],
+            'data' => $this->_data
+        ], JSON_FORCE_OBJECT);
+
+        header('Content-Type: application/json');
+        http_response_code($this->_responseCode);
+
+        echo $json;
+    }
+}
