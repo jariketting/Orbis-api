d5c5297bea81fc82f7ad409c993feba6dba5925c
Jari <personal@jariketting.com>
2019-06-02 17:19:20 +0200

    Add session check from database

--- src/Session.php
+++ src/Session.php
@@ -1,18 +1,21 @@
 <?php
 namespace Orbis;
 
+use PDO;
+
 /**
  * Session model class
  *
  * Class Session
  * @package Orbis
  */
-class Session extends Model
+class Session
 {
     /**
      * Validates session id
      */
     static function validate() : void {
+        $db = Database::get(); //get database connection
         $data = ['valid' => false]; //store return data
 
         $sessionId = Post::get('session_id'); //get session id from post
@@ -21,8 +24,30 @@ class Session extends Model
         if(!$sessionId)
             JsonResponse::error('Session id not given.', '"session_id" is missing in post data.', 400);
 
+        //TODO add session id validation
+
         //return session id (for debugging)
         $data['session_id'] = $sessionId;
+
+        /**
+         * Check if session id exists in database
+         */
+        $query = $db->prepare('
+            SELECT COUNT(*) AS count
+            FROM session 
+            WHERE id = :id 
+            LIMIT 1
+        ');
+        $query->bindParam(':id', $sessionId, PDO::PARAM_STR);
+        $query->execute();
+
+        if($query)
+            $result = $query->fetch(PDO::FETCH_OBJ)->count;
+        else
+            JsonResponse::error();
+
+        if($result)
+            $data['valid'] = true;
 
         JsonResponse::setData($data); //set data to created return data
     }
