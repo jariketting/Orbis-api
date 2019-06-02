6375b9c61dc08a239c778825eba00c227b8e02f9
Jari <personal@jariketting.com>
2019-06-02 17:00:00 +0200

    Add PHPDoc

--- src/Post.php
+++ src/Post.php
@@ -1,14 +1,27 @@
 <?php
 namespace Orbis;
 
-
+/**
+ * Simple class to get post params with some validation
+ *
+ * Less is more :)
+ *
+ * Class Post
+ * @package Orbis
+ */
 class Post
 {
+    /**
+     * @param $name
+     *
+     * @return string
+     */
     static function get($name) {
+        //validate post
         if(isset($_POST))
             if(isset($_POST[$name]))
-                return $_POST[$name];
+                return $_POST[$name]; //return post param
 
-        return '';
+        return ''; //return empty string on default
     }
 }
