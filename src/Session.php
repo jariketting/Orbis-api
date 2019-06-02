a0acb027160329eb86a710d30cb5092e11e17e69
Jari <personal@jariketting.com>
2019-06-02 16:58:43 +0200

    Add PHPDoc

--- src/Session.php
+++ src/Session.php
@@ -8,15 +8,17 @@ class Session extends Model
      * Validates session id
      */
     static function validate() : void {
-        $data = ['valid' => false];
+        $data = ['valid' => false]; //store return data
 
-        $sessionId = Post::get('session_id');
+        $sessionId = Post::get('session_id'); //get session id from post
 
+        //give error when no session id is given
         if(!$sessionId)
             JsonResponse::error('Session id not given.', '"session_id" is missing in post data.', 400);
 
+        //return session id (for debugging)
         $data['session_id'] = $sessionId;
 
-        JsonResponse::setData($data);
+        JsonResponse::setData($data); //set data to created return data
     }
 }
