fe09f2ff502525e1516acc01d664d4a2f78ad579
Jari <personal@jariketting.com>
2019-06-02 14:20:44 +0200

    Add PHPDoc

--- src/Router.php
+++ src/Router.php
@@ -9,24 +9,32 @@ namespace Orbis;
  */
 class Router
 {
-    private static  $_request = null;
+    private static  $_request = null; //stores raw request, null means not initialized
 
-    private static  $_type,
-                    $_action,
-                    $_identifier;
+    private static  $_type, //stores request type
+                    $_action, //stores request action, must be get, add, update or delete
+                    $_identifier; //stores identifier which is always an int
 
-    private static function _init() {
+    /**
+     * Initialize router
+     */
+    private static function _init() : void {
+        //check if redirect url isset
         if(isset($_SERVER['REDIRECT_URL'])) self::$_request = $_SERVER['REDIRECT_URL'];
         else self::$_request = '';
 
+        //if request is just a slash, set it to empty string
         if(self::$_request == '/') self::$_request = '';
 
+        //explode raw params
         $params = explode('/', self::$_request);
 
+        //set type, action and identifier
         self::$_type = (string)((isset($params[1])) ? $params[1] : '');
         self::$_action = (string)((isset($params[2])) ? $params[2] : '');
         self::$_identifier = (int)((isset($params[3])) ? $params[3] : '');
 
+        //make sure action is valid
         if( self::$_action != 'get'     &&
             self::$_action != 'update'  &&
             self::$_action != 'add'     &&
@@ -37,18 +45,33 @@ class Router
         }
     }
 
+    /**
+     * Get request type
+     *
+     * @return string
+     */
     public static function getType() : string {
         if(self::$_request === null) self::_init();
 
         return self::$_type;
     }
 
+    /**
+     * Get request action
+     *
+     * @return string
+     */
     public static function getAction() : string {
         if(self::$_request === null) self::_init();
 
         return self::$_action;
     }
 
+    /**
+     * Get request identifier
+     *
+     * @return int
+     */
     public static function getIdentifier() : int {
         if(self::$_request === null) self::_init();
 
