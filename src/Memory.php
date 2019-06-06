cd6de7f84281b9c4954154084eab6f1c4f84d17e
Jari <personal@jariketting.com>
2019-06-06 12:39:36 +0200

    Add user id to memory

--- src/Memory.php
+++ src/Memory.php
@@ -103,6 +103,9 @@ class Memory extends Model {
 
     private static function addRequest() : void {
         $memory = new Memory(0);
+
+        $memory->id = Session::getUser()->id;
+
         $memory->create(); //create user
         $memory->bindFields(); //bind fields
 
